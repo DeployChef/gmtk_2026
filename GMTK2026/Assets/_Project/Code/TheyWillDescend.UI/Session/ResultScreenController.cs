ecf33429994391f1a4f0ed3e7235ce61f9f0229f
Chef <dc.titovvlad@gmail.com>
2026-07-26 16:50:27 +0300

    проигрыш

--- GMTK2026/Assets/_Project/Code/TheyWillDescend.UI/Session/ResultScreenController.cs
+++ GMTK2026/Assets/_Project/Code/TheyWillDescend.UI/Session/ResultScreenController.cs
@@ -1,8 +1,10 @@
 using System;
+using Cysharp.Threading.Tasks;
 using TheyWillDescend.Core;
 using TheyWillDescend.Core.Audio;
 using TheyWillDescend.Core.Bus;
 using TheyWillDescend.Core.Bus.Events;
+using TheyWillDescend.Core.Session;
 using UnityEngine;
 using VContainer;
 
@@ -10,6 +12,7 @@ namespace TheyWillDescend.UI.Session
 {
     /// <summary>
     /// Shows Win/Lose canvases when GameWonEvent / GameLostEvent are published.
+    /// Timer-expire lose plays <see cref="LoseSequenceDriver"/> before the lose canvas.
     /// </summary>
     public sealed class ResultScreenController : MonoBehaviour
     {
@@ -17,6 +20,7 @@ namespace TheyWillDescend.UI.Session
         [SerializeField] private CanvasGroup loseCanvas;
         [SerializeField] private UnityEngine.UI.Button winRestartButton;
         [SerializeField] private UnityEngine.UI.Button loseRestartButton;
+        [SerializeField] private LoseSequenceDriver loseSequence;
 
         private IGameEventBus _bus;
         private IAudioManager _audio;
@@ -25,10 +29,19 @@ namespace TheyWillDescend.UI.Session
         private IDisposable _runStartedSub;
 
         [Inject]
-        public void Construct(IGameEventBus bus, IGameDirector director, IAudioManager audio)
+        public void Construct(
+            IGameEventBus bus,
+            IGameDirector director,
+            IAudioManager audio,
+            IGameplayTimePause timePause)
         {
             _bus = bus;
             _audio = audio;
+
+            if (loseSequence == null)
+                loseSequence = GetComponent<LoseSequenceDriver>();
+
+            loseSequence?.BindPause(timePause);
 
             if (winRestartButton != null)
                 winRestartButton.onClick.AddListener(() =>
@@ -45,7 +58,7 @@ namespace TheyWillDescend.UI.Session
                 });
 
             _wonSub = _bus.Subscribe<GameWonEvent>(_ => Show(winCanvas));
-            _lostSub = _bus.Subscribe<GameLostEvent>(_ => Show(loseCanvas));
+            _lostSub = _bus.Subscribe<GameLostEvent>(OnLost);
             _runStartedSub = _bus.Subscribe<RunStartedEvent>(_ =>
             {
                 Hide(winCanvas);
@@ -66,10 +79,42 @@ namespace TheyWillDescend.UI.Session
             _runStartedSub?.Dispose();
         }
 
+        private void OnLost(GameLostEvent e)
+        {
+            // Cheat + timer expire both play the meteor sequence when driver is present.
+            if (loseSequence != null &&
+                (e.Cause == GameResultCause.PyramidTimerExpired || e.Cause == GameResultCause.Cheat))
+            {
+                PlayLoseSequenceThenShow().Forget();
+                return;
+            }
+
+            Show(loseCanvas);
+        }
+
+        private async UniTaskVoid PlayLoseSequenceThenShow()
+        {
+            try
+            {
+                await loseSequence.PlayAsync(destroyCancellationToken);
+            }
+            catch (OperationCanceledException)
+            {
+                // Scene unload / destroy.
+            }
+
+            Show(loseCanvas);
+        }
+
         private static void Show(CanvasGroup cg)
         {
             if (cg == null)
                 return;
+
+            var canvas = cg.GetComponent<Canvas>();
+            if (canvas != null)
+                canvas.sortingOrder = Mathf.Max(canvas.sortingOrder, 100);
+
             cg.alpha = 1f;
             cg.interactable = true;
             cg.blocksRaycasts = true;
